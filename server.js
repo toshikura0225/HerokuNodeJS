21f762d2f03d808651ed8989852dd77450b243bc
toshikura <t_kuramata@yahoo.co.jp>
2015-02-07 14:23:16 +0900

    first commit

---
+++ server.js
@@ -0,0 +1,11 @@
+﻿var fs = require('fs');
+var app = require('http').createServer(function(req, res) {
+  res.writeHead(200, {'Content-Type': 'text/html'});
+  res.end(fs.readFileSync('index.html'));  
+}).listen(process.env.PORT || 3000);
+var io = require('socket.io').listen(app);
+io.sockets.on('connection', function(socket) {
+  socket.on('msg', function(data) {
+    io.sockets.emit('msg', data);
+  });
+});
