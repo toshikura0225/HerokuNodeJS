30261c1726875f52819c2551222ba2b3a0df5a5a
toshikura <t_kuramata@yahoo.co.jp>
2015-03-13 23:54:47 +0900

    solve?

--- server.js
+++ server.js
@@ -1,8 +1,31 @@
+var http = require('http');
 var fs = require('fs');
-var app = require('http').createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.end(fs.readFileSync('index.html'));  
+var url = require('url');
+
+var http_src = fs.readFileSync('./index.html');
+var js_src = fs.readFileSync('./js/my_script.js');
+
+var app = http.createServer(function(req, res) {
+	
+	var url_parts = url.parse(req.url);
+	
+	console.log(url_parts.pathname);
+	
+	if()url_parts.pathname == '/index.html')
+	{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(http_src);
+		res.end();
+	}
+	else if(url_parts.pathname == '/js/my_script.js')
+	{
+		res.writeHead(200, {'Content-Type': 'text/javascript'});
+		res.write(js_src);
+		res.end();
+	}
+	
 }).listen(process.env.PORT || 3000);
+
 var io = require('socket.io').listen(app);
 io.sockets.on('connection', function(socket) {
   socket.on('msg', function(data) {
