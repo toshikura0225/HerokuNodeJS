e8fb11010940141cdf757d31c10647b211e48030
toshikura <t_kuramata@yahoo.co.jp>
2015-03-15 18:03:03 +0900

    json communication sample

--- js/my_script.js
+++ js/my_script.js
@@ -2,11 +2,33 @@ $(function() {
 
 	var socket = io.connect();
 	$('form').submit(function() {
-		socket.emit('msg', $('input').val());
-		$('input').val('');
+		//socket.emit('msg', $('input').val());
+		//$('input').val('');
+		
+		var data = 
+		[{
+			"division": "asd",
+			"person": [
+				{ "name": 'asdf', "age": 21 },
+				{ "name": 'fse', "age": 56 },
+				{ "name": 'fs', "age": 33 }
+			]
+		},
+		{
+			"division": "sfe",
+			"person": [
+				{ "name": 'fgr', "age": 44 },
+				{ "name": 'fse', "age": 19 },
+				{ "name": 'se', "age": 26 }
+			]
+		}];
+		
+		socket.emit('msg', JSON.stringify(data));
 		return false;
 	});
 	socket.on('msg', function(data) {
+		var obj = jQuery.parseJSON(data);
+		alert(obj[0].division);
 		$('div').prepend(data + '<br>');
 	});
 	
