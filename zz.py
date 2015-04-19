2535f12c31675dcc9767e39c8d9f6415d8c49f4a
tablet <t_kuramata@yahoo.co.jp>
2015-04-19 17:19:17 +0900

    serial .py added

---
+++ zz.py
@@ -0,0 +1,30 @@
+# header comment
+
+from serial import Serial
+
+com = Serial(
+  port="/dev/ttyUSB0",
+  baudrate=9600,
+  bytesize=8,
+  parity='N',
+  stopbits=1,
+  timeout=1,
+  xonxoff=0,
+  rtscts=0,
+  writeTimeout=None,
+  dsrdtr=None)
+print(com.portstr)
+com.write(b"\x04")
+com.write("00ZZ")
+com.write(b"\x05")
+
+data = com.read(1000)
+print(len(data))
+
+com.close()
+
+f = open("data.txt", "w")
+for var in data:
+	print("%s" % str(var))
+	f.write("var=" + str(var))
+f.close()
