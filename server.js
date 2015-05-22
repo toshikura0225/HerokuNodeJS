cc7f7f52fc98123c6df639ad856d960b69f7eb4c
tablet <t_kuramata@yahoo.co.jp>
2015-05-22 21:55:26 +0900

    modosu server

--- server.js
+++ server.js
@@ -1,81 +1,50 @@
 var http = require('http');
 var fs = require('fs');
 var url = require('url');
-var path = require('path');
 
-var g_key = "default";
+var http_src = fs.readFileSync('./index.html');
+var js_src = fs.readFileSync('./js/my_script.js');
 
-var app = http.createServer(function(req, res)
-{
+var rasp_src= fs.readFileSync('./rasp.html');
+var ando_src= fs.readFileSync('./ando.html');
+
+var app = http.createServer(function(req, res) {
 	
 	var url_parts = url.parse(req.url);
 	
-	console.log("★★★" + url_parts.pathname);
+	console.log(url_parts.pathname);
 	
-	if (path.existsSync("." + url_parts.pathname))
-	{ 
-		var src= fs.readFileSync("." + url_parts.pathname);
-		res.writeHead(200);
-		res.write(src);
-		res.end();
-	}
-	else
+	if(url_parts.pathname == '/')
 	{
-		res.writeHead(200);
-		res.write("Nooooooooooot found!");
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(http_src);
 		res.end();
 	}
-
+	else if(url_parts.pathname == '/js/my_script.js')
+	{
+		res.writeHead(200, {'Content-Type': 'text/javascript'});
+		res.write(js_src);
+		res.end();
+	}
+	else if(url_parts.pathname == '/rasp.html')
+	{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(rasp_src);
+		res.end();
+	}
+	else if(url_parts.pathname == '/ando.html')
+	{
+		res.writeHead(200, {'Content-Type': 'text/html'});
+		res.write(ando_src);
+		res.end();
+	}
 	
 }).listen(process.env.PORT || 3000);
 
 var io = require('socket.io').listen(app);
-io.sockets.on('connection', function(socket)
-{
-	
-	socket.on('msg', function(data)
-	{
-		//io.sockets.emit('msg', data);
-		socket.broadcast.emit('msg', data);
-	});
-  
-	socket.on('ok', function(data)
-	{
-		//io.sockets.emit('msg', data);
-		socket.broadcast.emit('ok', data);
-	});
-  
-	socket.on('polling', function(data)
-	{
-		//io.sockets.emit('msg', data);
-		socket.broadcast.emit('polling', data);
-	});
-  
-	socket.on('selecting', function(data)
-	{
-		
-		//io.sockets.emit('msg', data);
-		console.log("======================" + data);
-		var obj = JSON.parse(data);
-		
-		if( obj.key == g_key)
-		{
-			console.log("======================" + obj.key);
-			console.log("======================" + obj.sequence);
-		
-			socket.broadcast.emit('selecting', obj.sequence);
-		}
-		else
-		{
-			// DO NOTHING...
-			console.log("============invalid key==========");
-		}
-	});
-	
-	socket.on('write_key', function(data)
-	{
-		g_key = data;
-		console.log("======================key registerd : " + data);
-	});
-	
+io.sockets.on('connection', function(socket) {
+  socket.on('msg', function(data) {
+    //io.sockets.emit('msg', data);
+	socket.broadcast.emit('msg', data);
+  });
 });
